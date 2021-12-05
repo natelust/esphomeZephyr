62fb341fbd42a36810462414033d3bcbae159c93
Nate Lust <nlust@astro.princeton.edu>
2021-12-05 12:44:37 -0500

    Add an OpenThread SRP component
    Add a component to control advertising the project over link local.
    This is similar to mDNS, but publishes records for the border router
    to actually do the advertising.

---
+++ esphome/components/openthread_srp/ot_srp.cpp
@@ -0,0 +1,128 @@
+#include "ot_srp.h"
+#include "esphome/core/log.h"
+
+#include <openthread/thread.h>
+#include <openthread/srp_client_buffers.h>
+#include <openthread/netdata.h>
+
+namespace esphome {
+namespace openthread {
+
+void OpenThreadSRP::setup() {
+    uint16_t len = host_name.size();
+    otError error;
+    int aborted = 1;
+    uint16_t size;
+    char *   existing_host_name;
+    uint8_t       arrayLength;
+    otIp6Address *hostAddressArray;
+    std::string service = "_esphomelib._tcp";
+    otSrpClientBuffersServiceEntry *entry = nullptr;
+    char *                          string;
+    char *                          label;
+    const otIp6Address * localIp = nullptr;
+    const otIp6Prefix * omrPrefix = nullptr;
+    otBorderRouterConfig aConfig;
+    otNetworkDataIterator iterator = OT_NETWORK_DATA_ITERATOR_INIT;
+    const otNetifAddress *unicastAddrs = nullptr;
+
+    uint8_t ip_found = 0;
+    char addressAsString[40];
+
+    struct openthread_context * context = openthread_get_default_context();
+    // lock the mutex
+    openthread_api_mutex_lock(context);
+
+    // set the host name
+    existing_host_name = otSrpClientBuffersGetHostNameString(context->instance, &size);
+
+    if (len > size) { 
+        ESP_LOGW("OT SRP", "Hostname is too long, choose a shorter project name");
+        goto exit;
+    }
+
+    memcpy(existing_host_name, host_name.c_str(), len + 1);
+    error = otSrpClientSetHostName(context->instance, existing_host_name);
+    if (error != 0){
+        ESP_LOGW("OT SRP", "Could not set host name with srp server");
+        goto exit;
+    }
+
+    // set the ip address
+    // get the link local ip address
+    //localIp = otThreadGetLinkLocalIp6Address(context->instance);
+    //localIp = otThreadGetMeshLocalEid(context->instance);
+    while (otNetDataGetNextOnMeshPrefix(context->instance, &iterator, &aConfig) != OT_ERROR_NONE) {
+        openthread_api_mutex_unlock(context);
+        k_msleep(100);
+        openthread_api_mutex_lock(context);
+    };
+    if (error != 0){
+        ESP_LOGW("OT SRP", "Could not get the OMR prefix");
+        goto exit;
+    }
+    omrPrefix = &aConfig.mPrefix;
+    otIp6PrefixToString(omrPrefix, addressAsString, 40);
+    ESP_LOGW("OT SRP", "USING omr prefix %s", addressAsString);
+    unicastAddrs = otIp6GetUnicastAddresses(context->instance);
+    for (const otNetifAddress *addr = unicastAddrs; addr; addr = addr->mNext){
+        localIp = &addr->mAddress;
+        if (otIp6PrefixMatch(&omrPrefix->mPrefix, localIp)) {
+            ip_found = 1;
+            otIp6AddressToString(localIp, addressAsString, 40);
+            ESP_LOGW("OT SRP", "USING %s for SRP address", addressAsString);
+            break;
+        }
+    }
+    if (ip_found == 0) {
+        ESP_LOGW("OT SRP", "Could not find the OMR address");
+        goto exit;
+    }
+
+    hostAddressArray = otSrpClientBuffersGetHostAddressesArray(context->instance, &arrayLength);
+
+    memcpy(hostAddressArray, localIp, sizeof(*localIp));
+    error = otSrpClientSetHostAddresses(context->instance, hostAddressArray, arrayLength);
+    if (error != 0){
+        ESP_LOGW("OT SRP", "Could not set ip address with srp server");
+        goto exit;
+    }
+
+    // set the records
+    entry = otSrpClientBuffersAllocateService(context->instance);
+    entry->mService.mPort = 6053;
+
+    string = otSrpClientBuffersGetServiceEntryInstanceNameString(entry, &size);
+    memcpy(string, host_name.c_str(), strlen(host_name.c_str()));
+
+    string = otSrpClientBuffersGetServiceEntryServiceNameString(entry, &size);
+    memcpy(string, service.c_str(), strlen(service.c_str()));
+
+    label = strchr(string, ',');
+    entry->mService.mNumTxtEntries = 0;
+    error = otSrpClientAddService(context->instance, &entry->mService);
+    if (error != 0){
+        ESP_LOGW("OT SRP", "Could not set service to advertise.");
+        goto exit;
+    }
+
+    otSrpClientEnableAutoStartMode(context->instance, nullptr, nullptr);
+    // unlock the mutex
+    aborted = 0;
+exit:
+    if (aborted) {
+        this->mark_failed();
+        ESP_LOGW("OT SRP", "Setting SRP record failed, clear partial state");
+        k_msleep(100);
+        otSrpClientClearHostAndServices(context->instance);
+        otSrpClientBuffersFreeAllServices(context->instance);
+    }
+    openthread_api_mutex_unlock(context);
+}
+
+void OpenThreadSRP::set_host_name(std::string host_name){
+    this->host_name = host_name;
+}
+
+}  // namespace openthread
+}  // namespace esphome
---
+++ esphome/components/openthread_srp/ot_srp.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "esphome/core/component.h"
+
+#include<string>
+
+#include <net/openthread.h>
+#include <openthread/srp_client.h>
+
+namespace esphome {
+namespace openthread {
+
+class OpenThreadSRP : public Component {
+    public:
+        void setup() override;
+        float get_setup_priority() const override {
+            return setup_priority::HARDWARE;
+        }
+        void set_host_name(std::string host_name);
+    protected:
+    std::string host_name;
+};
+
+}  // namespace openthread
+}  // namespace esphome
