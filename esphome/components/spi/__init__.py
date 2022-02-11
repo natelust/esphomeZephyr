1bb9e7f506838678163fc0da4676eb7b5aaad3dd
Nate Lust <nlust@astro.princeton.edu>
2022-02-11 01:11:52 -0500

    SPI component improvements for Zephyr

--- esphome/components/spi/__init__.py
+++ esphome/components/spi/__init__.py
@@ -48,14 +48,15 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    clk = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
-    cg.add(var.set_clk(clk))
-    if CONF_MISO_PIN in config:
-        miso = await cg.gpio_pin_expression(config[CONF_MISO_PIN])
-        cg.add(var.set_miso(miso))
-    if CONF_MOSI_PIN in config:
-        mosi = await cg.gpio_pin_expression(config[CONF_MOSI_PIN])
-        cg.add(var.set_mosi(mosi))
+    if not CORE.is_zephyr:
+        clk = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
+        cg.add(var.set_clk(clk))
+        if CONF_MISO_PIN in config:
+            miso = await cg.gpio_pin_expression(config[CONF_MISO_PIN])
+            cg.add(var.set_miso(miso))
+        if CONF_MOSI_PIN in config:
+            mosi = await cg.gpio_pin_expression(config[CONF_MOSI_PIN])
+            cg.add(var.set_mosi(mosi))
 
     if CORE.using_arduino:
         cg.add_library("SPI", None)
